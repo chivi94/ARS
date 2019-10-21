b1039141b48aca6e2db07600bc701eceadc4898a
Ivan <ivan.gonzalez.rincon>
2019-10-21 16:35:38 +0200

    Se ha llegado hasta el envío de datos. No deja conectar con el servidor porque la ip es privada y sólo deja desde la MV

--- Practica1/daytime-udp-client-Gonzalez-Rincon.c
+++ Practica1/daytime-udp-client-Gonzalez-Rincon.c
@@ -16,6 +16,15 @@ void error(char * message){
 	exit(EXIT_FAILURE);
 }
 
+void closeSocket(int result){
+//Comprobamos si al cerrar el socket algo va mal, notificando al usuario de ser asi.
+	int closeResult = close(result);
+	if(closeResult <0){
+		error("Error al cerrar el socket\n");
+	}
+	exit(EXIT_FAILURE);
+}
+
 int main(int argc, char *argv[]){
 	printf("Numero de argumentos: %i\n",argc);
 	printf("Direccion:%s\n",argv[1]);
@@ -29,7 +38,7 @@ int main(int argc, char *argv[]){
 	}
 	/*Despues de comprobar la cantidad de argumentos, se comprueba
 	si se ha introducido un puerto del servidor*/
-	int puerto;
+	int puerto = 0;
 	if(argc >=3){
 		if(strcmp("-p", argv[2])==0){
 			//Cogemos el numero de puerto del usuario
@@ -60,22 +69,34 @@ int main(int argc, char *argv[]){
 	printf("El socket se ha creado correctamente\n");
 
 	//Ahora vamos a enlazar el socket
-	int bindingResult, closeResult;
+	int bindingResult;
 	struct sockaddr_in myaddr;
 	myaddr.sin_family = AF_INET;
 	myaddr.sin_port = 0;
 	myaddr.sin_addr.s_addr = INADDR_ANY;
 
+	//Comprobamos si surge algun error al enlazar el socket a la direccion local
 	if((bindingResult = bind(socketResult, (struct sockaddr *) &myaddr, sizeof(myaddr))) < 0){
+		//De haberlo, se notifica, e intentamos cerrar el Socket
 		error("Error al enlazar el socket\n");
-		closeResult = close(socketResult);
-		if(closeResult <0){
-			error("Error al cerrar el socket\n");
-		}
-		exit(EXIT_FAILURE);
+		//Comprobamos si al cerrar el socket algo va mal, notificando al usuario de ser asi.
+		closeSocket(socketResult);
 	}
 	printf("El socket se ha enlazado correctamente\n");
-	
+
+	//Vamos a mandar datos al servidor
+	int sendResult;
+	char datos [] = "Dame el dia actual";
+	struct sockaddr_in serverAddr;
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = 0 ;
+	serverAddr.sin_addr = address;
+
+	sendResult = sendto(socketResult, datos, sizeof(datos), 0, (struct sockaddr *)&serverAddr,sizeof(serverAddr));
+	if(sendResult < 0){
+		error("Error al enviar datos al servidor\n");
+		closeSocket(sendResult);
+	}
 	//if(connect())
 
 
