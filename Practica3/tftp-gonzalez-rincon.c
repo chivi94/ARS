4de6c73036f624613a2dc53324afbc7231f305dc
Ivan <ivan.gonzalez.rincon>
2019-11-28 13:33:49 +0100

    Se ha implementado hasta la escritura del cliente. Falta una parte aún

--- Practica3/tftp-gonzalez-rincon.c
+++ Practica3/tftp-gonzalez-rincon.c
@@ -13,23 +13,55 @@
 
 #define SIZEBUFF 512
 #define HOSTNAME 512
-#define MINARGS 2
+#define PACKAGEPART 512
+#define PACKAGETORCV 516
+#define BYTESTOADD 2
+/*
+Argumentos obligatorios:
+- Nombre del programa.
+- IP del servidor.
+- Modo del programa (lectura o escritura).
+- Archivo sobre el que leer o escribir.
+*/
+#define MINARGS 4
+/*
+Maximo numero de argumentos:
+- Los anteriores +
+    - Modo verbose.
+    - Ayuda para el usuario.
+*/
 #define MAXARGS 6
+
+/**/
+#define TRANSMISSIONMODE "octet"
 
 //Cabeceras de funciones
 void checkArguments(int argc, char *argv[]);
 void error(char message[]);
 void closeSocket(int result);
 void help();
+void verboseText(int programPart);
+void checkResult(int result, char message[]);
+unsigned char *initPackage();
+unsigned char *checkPckg(int pckgSize, unsigned char *package, int blockNumber);
 void readMode(int socketResult);
 void writeMode(int socketResult);
 
 //Variables globales necesarias para el programa
 int socketResult;
 int communicationMode;
-FILE *fich;
+int pckgSize = 0;
+char *nameOfFile;
 char bufferOut[SIZEBUFF];
-uint16_t server;
+uint16_t serverPort;
+struct in_addr serverIPAddress;
+FILE *fichOut;
+FILE *fichIn;
+/*
+Esta variable se usara para activar el modo paso a paso(verbose), 
+solicitado por el enunciado de la practica. Puesta la variable
+a valor 1(true), indica que este modo se encuentra activado*/
+int verboseMode = 0;
 
 /*
 Formato de invocacion: client IP {-r|-w} archivo [-v][-h].
@@ -68,7 +100,7 @@ int main(int argc, char *argv[])
         error("Error al bindear el puerto.\n");
     }
 
-    server = defaultPort->s_port;
+    serverPort = defaultPort->s_port;
 
     //Una vez obtenido el numero de puerto, procedemos a establecer la conexion con el socket
     int socketResult;
@@ -76,44 +108,54 @@ int main(int argc, char *argv[])
     {
         error("Error al crear el socket.\n");
     }
-    
-    //Una vez creado el socket, procedemos a bindearlo.
-	int bindingResult;
-	struct sockaddr_in myaddr;
-	myaddr.sin_family = AF_INET;
-	myaddr.sin_port = 0;
-	myaddr.sin_addr.s_addr = INADDR_ANY;
 
-	//Comprobamos si surge algun error al enlazar el socket a la direccion local
-	if((bindingResult = bind(socketResult, (struct sockaddr *) &myaddr, sizeof(myaddr))) < 0){
-		//De haberlo, se notifica, e intentamos cerrar el Socket
-		error("Error al enlazar el socket\n");
-		//Comprobamos si al cerrar el socket algo va mal, notificando al usuario de ser asi.
-		closeSocket(socketResult);
-	}
+    //Una vez creado el socket, procedemos a bindearlo.
+    int bindingResult;
+    struct sockaddr_in clientAddress;
+    clientAddress.sin_family = AF_INET;
+    clientAddress.sin_port = 0;
+    clientAddress.sin_addr.s_addr = INADDR_ANY;
+
+    //Comprobamos si surge algun error al enlazar el socket a la direccion local
+    if ((bindingResult = bind(socketResult, (struct sockaddr *)&clientAddress, sizeof(clientAddress))) < 0)
+    {
+        //De haberlo, se notifica, e intentamos cerrar el Socket
+        error("Error al enlazar el socket\n");
+        //Comprobamos si al cerrar el socket algo va mal, notificando al usuario de ser asi.
+        closeSocket(socketResult);
+    }
 
     //Una vez enlazado todo, comenzamos la comunicacion
-    switch(communicationMode){
-        case 01:
-            readMode(socketResult);
-            break;
-        case 02:
-            writeMode(socketResult);
-            break;
-        default:
-            error("Fallo en la ejecucion del programa.\n");
-            break;
+    switch (communicationMode)
+    {
+    case 01:
+        readMode(socketResult);
+        break;
+    case 02:
+        writeMode(socketResult);
+        break;
+    default:
+        error("Fallo en la ejecucion del programa.\n");
+        break;
     }
     closeSocket(socketResult);
     exit(EXIT_SUCCESS);
 }
 
 //Metodo para comprobar los parametros de entrada
-void checkArguments(int argc, char *argv[]){
+void checkArguments(int argc, char *argv[])
+{
     int i;
     // Iteramos sobre los argumentos para ver cuales han entrado.
     for (i = 1; i < argc; i++)
     {
+        if (i == 1)
+        {
+            if ((inet_aton(argv[i], &serverIPAddress)) <= 0)
+            {
+                error("Conversion IP.\n");
+            }
+        }
         //Modo lectura
         if (strcmp("-r", argv[i]) == 0)
         {
@@ -127,6 +169,7 @@ void checkArguments(int argc, char *argv[]){
         //Verbose
         if (strcmp("-v", argv[i]) == 0)
         {
+            verboseMode = 1;
         }
         //Ayuda
         if (strcmp("-h", argv[i]) == 0)
@@ -156,6 +199,7 @@ void closeSocket(int result)
     exit(EXIT_FAILURE);
 }
 
+//Metodo para mostrar ayuda al usuario sobre el uso del comando
 void help()
 {
     printf("El uso del programa es el siguiente:\n");
@@ -165,4 +209,149 @@ void help()
     printf("\t- archivo: Nombre del archivo con el que trabajaremos.\n");
     printf("\t- [-v]: Modo verbose en el que se muestra una traza del cliente.\n");
     printf("\t- [-h]]: Muestra de ayuda para ver como usar el programa.\n");
+}
+
+/*Este metodo se encargara de imprimir el texto del modo verbose
+dependiendo de las distintas partes del programa donde se llame.
+*/
+void verboseText(int programPart)
+{
+    switch (programPart)
+    {
+    case 1:
+        printf("Enviada solicitud de lectura de %s a servidor tftp en %s\n", nameOfFile, inet_ntoa(serverIPAddress));
+        break;
+    case 2:
+        break;
+    case 3:
+        break;
+    case 4:
+        break;
+    case 5:
+        printf("Ultimo paquete mandado. Cerramos el fichero.\n");
+        break;
+    default:
+    }
+}
+
+void checkResult(int result, char message[]){
+    if (result < 0)
+        {
+            fclose(fichIn);
+            error(message);
+            closeSocket(socketResult);
+        }
+}
+
+/*
+Este metodo se encargara de la tarea de inicializar los paquetes
+de la aplicacion y formatearlos correctamente para que los datos
+se almacenen como corresponde.
+*/
+unsigned char *initPackage()
+{
+
+    pckgSize = 0;
+    unsigned char *resultPackage;
+    /*En este caso usamos calloc en lugar de malloc
+    porque nos interesa que se reserven espacios contiguos de memoria
+    correspondientes al tamanio maximo de cada subconjunto del paquete total
+    que se va a solicitar al servidor, y que todos esos espacios sean del mismo tipo.
+    */
+    resultPackage = (unsigned char *)(calloc(PACKAGEPART, sizeof(unsigned char)));
+    if (resultPackage == 0)
+    {
+        error("Fallo al reservar memoria.\n");
+    }
+
+    /*
+    A continuacion se crea el formato del paquete, 
+    siguiendo el indicado en el enunciado de la practica.
+    */
+    resultPackage[1] = communicationMode;
+    pckgSize = 2;
+    int addSize = sprintf((char *)(resultPackage + BYTESTOADD), "%s", nameOfFile);
+    if (addSize < 0)
+    {
+        error("Ha ocurrido un error al formatear el nombre del fichero.\n");
+    }
+    /*Las sumas que se realizan al tamanio del paquete se corresponden
+    a desplazamientos en los bytes necesarios para respetar
+    el formato de los paquetes de datos.
+    */
+    pckgSize = (pckgSize + addSize) + 1;
+
+    addSize = sprintf((char *)resultPackage + pckgSize, "%s", TRANSMISSIONMODE);
+    if (addSize < 0)
+    {
+        error("Fallo en el formato del paquete.\n");
+    }
+    pckgSize += addSize;
+    resultPackage[pckgSize] = 0;
+    pckgSize++;
+    return resultPackage;
+}
+
+/*Metodo que se encargara de comprobar los tipos de paquete que
+se enviann y/o reciben entre el cliente y el servidor, para poder
+llevar un control de los mismos.
+*/
+unsigned char *checkPckg(int pckgSize, unsigned char *package, int blockNumber){
+
+}
+
+//Metodo para activar el modo lectura del cliente
+void readMode(int socketResult)
+{
+    //Vamos a mandar datos al servidor
+    int sendResult, recvResult;
+    unsigned char *out;
+    unsigned char *in;
+
+    struct sockaddr_in serverAddr;
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = serverPort;
+    serverAddr.sin_addr = serverIPAddress;
+
+    out = initPackage();
+
+    //Si el modo verbose esta activado -> informacion.
+    if (verboseMode)
+    {
+        verboseText(1);
+    }
+
+    //Enviamos los datos y comprobamos si da fallo. En caso afirmativo, el programa termina.
+    sendResult = sendto(socketResult, out, pckgSize, 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+    if (sendResult < 0)
+    {
+        error("Error al enviar datos al servidor\n");
+        closeSocket(socketResult);
+    }
+
+    if ((in = (unsigned char *)(calloc(PACKAGETORCV, sizeof(unsigned char))) == 0))
+    {
+        error("Reserva en la memoria fallida para los datos provinientes del servidor.\n");
+    }
+
+    socklen_t addressLength = sizeof(serverAddr);
+    int blockNumber = 0;
+    //Comprobamos que se enlaza correctamente, cerrando la conexion en caso contrario.
+    do
+    {
+        recvResult = recvfrom(socketResult, &in, PACKAGETORCV, 0, (struct sockaddr *)&serverAddr, &addressLength);
+        checkResult(recvResult, "Error al recibir datos del servidor\n");        
+        if(out != 0){
+            free(out);
+        }
+
+        out = checkPackage(0, in, blockNumber);
+		sendResult = sendto(socketResult, out, pckgSize, 0, (struct sockaddr *)&serverAddr, sizeof(serverAddr));
+		checkResult(sendResult, "Error al enviar datos al servidor.\n");
+		blockNumber++;
+    } while (recvResult - 4 == PACKAGEPART);
+    if(verboseMode){
+        verboseText(5);
+    }
+    fclose(fichIn);
 }
