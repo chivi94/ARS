3691727985552f49e35429388384697cbce03630
Ivan <ivan.gonzalez.rincon>
2019-12-01 15:10:33 +0100

    implementada la escritura

--- Practica3/refactor.c
+++ Practica3/refactor.c
@@ -139,7 +139,7 @@ int main(int argc, char *argv[])
             readMode(socketResult);
             break;
         case 02:
-            //writeMode(socketResult);
+            writeMode(socketResult);
             break;
         default:
             error("Fallo en la ejecucion del programa.\n");
