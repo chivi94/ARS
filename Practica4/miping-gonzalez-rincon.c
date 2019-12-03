8d31023e1346ba4fe667c3144354c83162058e47
Ivan <ivan.gonzalez.rincon>
2019-12-03 12:31:14 +0100

    Añadido fichero con las estructuras de datos.

--- Practica4/miping-gonzalez-rincon.c
+++ Practica4/miping-gonzalez-rincon.c
@@ -1,6 +1,17 @@
 //Practica tema 8, Gonzalez Rincon Ivan
 //Cliente
 
-int main(int argc, char *argv[]){
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <sys/types.h>
+#include <sys/unistd.h>
+#include "ip-icmp-ping.h"
 
+int main(int argc, char *argv[]){
+    exit(EXIT_SUCCESS);
 }
